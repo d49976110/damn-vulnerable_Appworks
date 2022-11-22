440e2236cfd298a907b88955fc54b752b99b11e2
Brian <>
2022-11-22 19:27:15 +0800

    feat: finish 1 & 2

--- contracts/naive-receiver/FlashLoanReceiver.sol
+++ contracts/naive-receiver/FlashLoanReceiver.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Address.sol";
+import "hardhat/console.sol";
 
 /**
  * @title FlashLoanReceiver
@@ -23,17 +24,20 @@ contract FlashLoanReceiver {
 
         uint256 amountToBeRepaid = msg.value + fee;
 
-        require(address(this).balance >= amountToBeRepaid, "Cannot borrow that much");
-        
+        require(
+            address(this).balance >= amountToBeRepaid,
+            "Cannot borrow that much"
+        );
+
         _executeActionDuringFlashLoan();
-        
+
         // Return funds to pool
         pool.sendValue(amountToBeRepaid);
     }
 
     // Internal function where the funds received are used
-    function _executeActionDuringFlashLoan() internal { }
+    function _executeActionDuringFlashLoan() internal {}
 
     // Allow deposits of ETH
-    receive () external payable {}
+    receive() external payable {}
 }
